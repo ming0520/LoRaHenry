24a5d8eddb6a2224848ee48e4749e1bd25316321
Zhong Ming Tan <31769920+ming0520@users.noreply.github.com>
2023-05-20 23:52:21 +0800

    add version 5

---
+++ PacketLossV5/PacketLossSenderV5/PacketLossSenderV5.ino
@@ -0,0 +1,39 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+const int csPin = 10;
+const int resetPin = 9;
+const int irqPin = 2;
+const int packetCount = 100;  // Number of packets to send
+int sentCount = 0;
+int successCount = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  LoRa.setPins(csPin, resetPin, irqPin);
+  if (!LoRa.begin(433E6)) {
+    Serial.println("LoRa initialization failed. Check your connections.");
+    while (true);
+  }
+  Serial.println("LoRa Sender Start");
+}
+
+void loop() {
+  if (sentCount < packetCount) {
+    String packet = "Hello, packet number " + String(sentCount);
+
+    LoRa.beginPacket();
+    LoRa.print(packet);
+    LoRa.endPacket();
+
+    sentCount++;
+    Serial.println("Packet sent: " + packet);
+  } else {
+    Serial.println("Packet sending complete.");
+    while (true);
+  }
+
+  delay(1000);
+}
