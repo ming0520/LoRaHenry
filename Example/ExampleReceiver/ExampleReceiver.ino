56fbafad30e2bd2074f9d8edcdea38bad14eba47
Zhong Ming Tan <31769920+ming0520@users.noreply.github.com>
2023-05-21 01:37:37 +0800

    Example V2

--- Example/ExampleReceiver/ExampleReceiver.ino
+++ Example/ExampleReceiver/ExampleReceiver.ino
@@ -1,60 +1,67 @@
 // LORA code for Receiver Side
-
-
 #include <SPI.h>
-
 #include <LoRa.h>
 
+const int packetCount = 5;  // Number of packets to receive
+int receivedCount = 0;
+int successCount = 0;
+bool stopFlag = true;
 
 void setup() {
-
   Serial.begin(9600);
-
   while (!Serial);
-
 
   Serial.println("LoRa Receiver");
 
-
   if (!LoRa.begin(433E6)) {
-
     Serial.println("Starting LoRa failed!");
-
     while (1);
-
   }
-
 }
-
 
 void loop() {
 
-  // try to parse packet
-
-  int packetSize = LoRa.parsePacket();
-
-  if (packetSize) {
-
-    // received a packet
-
-    Serial.print("Received packet '");
-
-
-    // read packet
-
-    while (LoRa.available()) {
-
-      Serial.print((char)LoRa.read());
-
+  int packetSize;
+  while(stopFlag){
+    packetSize = LoRa.parsePacket();
+    if (packetSize) {
+      while (LoRa.available()) {
+        char receivedChar = (char)LoRa.read();
+        Serial.println(receivedChar);
+        if(receivedChar == '#'){
+          Serial.println("Break");
+          stopFlag = false;
+        }
+      }
     }
+  // delay(1000);
+  }
 
+  // try to parse packet
+    packetSize = LoRa.parsePacket();
 
-    // print RSSI of packet
+    if (packetSize) {
+      // received a packet
+      Serial.print("Received packet '");
+      // read packet
+      while (LoRa.available()) {
+        receivedCount = (int)LoRa.read()-(int)'0';
+        if (receivedCount < packetCount && receivedCount >= 0){
+          Serial.print(receivedCount);
+          successCount++;
+        }         
+      }
 
+      if(receivedCount == -6){
+        Serial.println("Packet receiving complete.");
+        Serial.println(receivedCount);
+        Serial.println("Success rate: " + String(successCount) + " out of " + String(packetCount));
+        while (true);       
+      }
+
+     // print RSSI of packet
     Serial.print("' with RSSI ");
-
-    Serial.println(LoRa.packetRssi());
-
+    Serial.println(LoRa.packetRssi());         
   }
 
 }
