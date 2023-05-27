481912a731ea59b167856afdaeac586529876976
Zhong Ming Tan <31769920+ming0520@users.noreply.github.com>
2023-05-27 16:29:44 +0800

    Update Example Receiver

--- Example/ExampleReceiver/ExampleReceiver.ino
+++ Example/ExampleReceiver/ExampleReceiver.ino
@@ -2,13 +2,13 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-const int packetCount = 20;  // Number of packets to receive
+const int packetCount = 9999;  // Number of packets to receive
 int receivedCount = 0;
 int successCount = 0;
-bool stopFlag = true;
+bool stopFlag = false;
 
 void setup() {
-  LoRa.setTxPower(txPower);
+  // LoRa.setTxPower(20);
   successCount = 0;
   receivedCount = 0;
   Serial.begin(9600);
@@ -20,26 +20,27 @@ void setup() {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
+  Serial.println("packet id,success count,RSSI,frequency error,SNR");
 }
 
 void loop() {
 
   int packetSize;
 
-  while(stopFlag){
-    packetSize = LoRa.parsePacket();
-    if (packetSize) {
-      while (LoRa.available()) {
-        char receivedChar = (char)LoRa.read();
-        Serial.println(receivedChar);
-        if(receivedChar == '#'){
-          Serial.println("Break");
-          stopFlag = false;
-        }
-      }
-    }
-  // delay(1000);
-  }
+  // while(stopFlag){
+  //   packetSize = LoRa.parsePacket();
+  //   if (packetSize) {
+  //     while (LoRa.available()) {
+  //       char receivedChar = (char)LoRa.read();
+  //       Serial.println(receivedChar);
+  //       if(receivedChar == '#'){
+  //         Serial.println("Break");
+  //         stopFlag = false;
+  //       }
+  //     }
+  //   }
+  // // delay(1000);
+  // }
 
   // try to parse packet
     packetSize = LoRa.parsePacket();
@@ -53,14 +54,18 @@ void loop() {
         // receivedCount =  String((char)LoRa.read()).toInt();
         
         // receivedCount = (int)LoRa.read()-(int)'0'; 
-        message += (char)LoRa.read();   
+        message += (char)LoRa.read();
+        // Serial.print((char)LoRa.read());
       }
       receivedCount = message.toInt();
       if (receivedCount < packetCount && receivedCount >= 0){
-        Serial.print("Received packet ");
+        // Serial.print("Received packet ");
+        // Serial.print(",");
         Serial.print(receivedCount);
         successCount = successCount + 1;
-        Serial.print(" Success Count:" + String(successCount));
+        // Serial.print(" Success Count:" + String(successCount));
+        Serial.print(",");
+        Serial.print(successCount);
       }else{
         Serial.println("Packet receiving complete.");
         Serial.println(receivedCount);
@@ -78,11 +83,11 @@ void loop() {
 
 
      // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
-    Serial.print("' Packet Frequency Error: ");
-    Serial.println(LoRa.packetFrequencyError()); 
-    Serial.print("' packetSnr ");
+    Serial.print(",");
+    Serial.print(LoRa.packetRssi());
+    Serial.print(",");
+    Serial.print(LoRa.packetFrequencyError()); 
+    Serial.print(",");
     Serial.println(LoRa.packetSnr());             
   }
 
