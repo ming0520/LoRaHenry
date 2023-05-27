3b9acb50642589b9bbad12bf9605441512d69e06
Zhong Ming Tan <31769920+ming0520@users.noreply.github.com>
2023-05-27 14:41:18 +0800

    Update KeepSending Sender

--- KeepSending/Sender/Sender.ino
+++ KeepSending/Sender/Sender.ino
@@ -1,53 +1,30 @@
-// LORA code for Transmitting Side
 #include <SPI.h>
 #include <LoRa.h>
 
-const int packetCount = 20;  // Number of packets to send
-int sentCount = 0;
+int counter = 0;
 
 void setup() {
-
   Serial.begin(9600);
   while (!Serial);
 
   Serial.println("LoRa Sender");
-
+  // LoRa.enableCrc();
   if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
-  LoRa.setTxPower(20);
-
-  LoRa.beginPacket();
-  Serial.println("#");
-  LoRa.print("#");
-  LoRa.endPacket();
-  delay(1000);
 }
 
 void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
 
-  if(sentCount < packetCount+1){
-    Serial.print("Sending packet: ");
-    Serial.println(sentCount);
+  // send packet
+  LoRa.beginPacket();
+  // LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+  counter++;
 
-    // send packet
-    LoRa.beginPacket();
-    LoRa.print(sentCount);
-    LoRa.endPacket();
-
-    sentCount++;
-  }else {
-    Serial.println("Packet sending complete.");
-    while (true){
-      // LoRa.beginPacket();
-      // Serial.println("*");
-      // LoRa.print("*");
-      // LoRa.endPacket();
-      // delay(1000);
-    }
-  }
-  
-  delay(1000);
-
+  delay(3000);
 }
